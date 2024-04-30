bed8ba4c665d1d4f85f42207de6fc63691082aa4
M N Sakib <m.n.sakib@brainstation-23.com>
2024-04-30 16:58:30 +0600

    3. Primitive Types and Expressions

--- Hello-World/Program.cs
+++ Hello-World/Program.cs
@@ -1,10 +1,15 @@
-﻿namespace Hello_World
+﻿using System;
+namespace Hello_World
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var number = 2;
+            var number2 = 30.00;
+            var number3 = 20.73f;
+            Console.WriteLine("Hello, World!"+ number);
+            Console.WriteLine("{0} {1}",byte.MinValue,byte.MaxValue);
         }
     }
 }
